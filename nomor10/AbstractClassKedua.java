17a55f9b0e37514ea88fc469dd79b3294f3a100e
Fajrin198 <fajrinpanji6969@gmail.com>
2023-11-13 21:44:27 +0800

    Fajrin

---
+++ nomor10/AbstractClassKedua.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nomor10;
+
+/**
+ *
+ * @author MY ASUS
+ */
+public abstract class AbstractClassKedua extends AbstractClassPertama {
+    public void method02(){
+        System.out.println("Berhasil extends class kedua");
+    }
+}
---
+++ nomor10/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package nomor10;
+
+/**
+ *
+ * @author MY ASUS
+ */
+public class Test {
+    public static void main(String[] args) {
+        ChildClass cc = new ChildClass();
+        cc.method01();
+        cc.method02();
+    }
+}
